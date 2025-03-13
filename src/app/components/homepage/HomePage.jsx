79a313d4ff0e27196a1aae472662cb62b70b4128
Mahbub8340 <mdmahbubalam573125@gmail.com>
2025-03-13 16:20:53 +0600

    Login Page Design

--- src/app/components/homepage/HomePage.jsx
+++ src/app/components/homepage/HomePage.jsx
@@ -3,18 +3,13 @@ import Banner from "./Banner";
 import StatsSection from "./StatsSection";
 
 const HomePage = () => {
-  return (
-    <div>
-<<<<<<< HEAD
-      <Banner />
-      <StatsSection />
-=======
-      <h1>This is home page</h1>
-      <Banner></Banner>
-      <AvailableCourse></AvailableCourse>
->>>>>>> c1e0efc41639aeb35dd26b84e167da6149d533ed
-    </div>
-  );
+    return (
+        <div>
+            <AvailableCourse></AvailableCourse>
+            <Banner />
+            <StatsSection />
+        </div>
+    );
 };
 
 export default HomePage;
