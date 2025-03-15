060dffa7c8acad374b91fcfe2452e7bd77be5474
tareq ahmed <tareqahmed347396@gmail.com>
2025-03-16 05:58:57 +0600

    error fixed and update something

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -6,8 +6,8 @@ const handler = NextAuth({
   providers: [
     // OAuth authentication providers...
     GithubProvider({
-      clientId: process.env.GITHUB_ID,
-      clientSecret: process.env.GITHUB_SECRET,
+      clientId: process.env.GITHUB_CLIENT_ID,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET,
     }),
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID,
