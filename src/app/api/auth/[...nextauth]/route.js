8d511988cb015c807f2026b6ed76630a0eaf9ab7
tareq ahmed <tareqahmed347396@gmail.com>
2025-03-16 06:34:26 +0600

    update github provider

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -6,16 +6,14 @@ const handler = NextAuth({
   providers: [
     // OAuth authentication providers...
     GithubProvider({
-      clientId: process.env.GITHUB_CLIENT_ID,
-      clientSecret: process.env.GITHUB_CLIENT_SECRET,
-    }),
-    GithubProvider({
-      clientId: process.env.GITHUB_CLIENT_ID_PROD,
-      clientSecret: process.env.GITHUB_CLIENT_SECRET_PROD,
-    }),
-    GithubProvider({
-      clientId: process.env.GITHUB_CLIENT_ID_PROD_2,
-      clientSecret: process.env.GITHUB_CLIENT_SECRET_PROD_2,
+      clientId:
+        process.env.NODE_ENV === "production"
+          ? process.env.GITHUB_CLIENT_ID_PROD
+          : process.env.GITHUB_CLIENT_ID,
+      clientSecret:
+        process.env.NODE_ENV === "production"
+          ? process.env.GITHUB_CLIENT_SECRET_PROD
+          : process.env.GITHUB_CLIENT_SECRET,
     }),
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID,
