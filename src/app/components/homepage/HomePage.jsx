2ec27a1c39330db24bed96892b68ce88c5fb3cdb
asraful-07 <asraful0508@gmail.com>
2025-03-25 16:41:20 +0600

    added new section

--- src/app/components/homepage/HomePage.jsx
+++ src/app/components/homepage/HomePage.jsx
@@ -1,11 +1,13 @@
-import Banner from './Banner';
-import CoursePlatform from './CoursePlatform';
-import Headline from './Headline';
-import PopularInsights from './PopularInsights';
-import Review from './Review';
-import StatsSection from './StatsSection';
-import StudentSuccessStories from './StudentSuccessStories';
-import TestimonialSection from './TestimonialSection';
+import Achievement from "./Achievement";
+import Banner from "./Banner";
+import CoursePlatform from "./CoursePlatform";
+import Headline from "./Headline";
+import HeroSection from "./HeroSection";
+import PopularInsights from "./PopularInsights";
+import Review from "./Review";
+import StatsSection from "./StatsSection";
+import StudentSuccessStories from "./StudentSuccessStories";
+import TestimonialSection from "./TestimonialSection";
 
 const HomePage = () => {
   return (
@@ -13,11 +15,13 @@ const HomePage = () => {
       <div>
         <Banner />
         <Headline />
+        <HeroSection />
         <CoursePlatform></CoursePlatform>
         <Review />
         <PopularInsights></PopularInsights>
         <StatsSection />
         <StudentSuccessStories></StudentSuccessStories>
+        <Achievement />
         <TestimonialSection></TestimonialSection>
       </div>
     </div>
